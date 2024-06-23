96b4676aea382c9415834106d8fc70c39f69722a
JullMann <julia.mannanova@gmail.com>
2024-06-24 00:53:04 +0300

    Создан общий базовый класс HelperBase

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -3,18 +3,66 @@ package manager;
 import model.GroupData;
 import org.openqa.selenium.By;
 
-public class GroupHelper {
-
-    private final ApplicationManager manager;
+public class GroupHelper extends HelperBase {
 
     public GroupHelper(ApplicationManager manager) {
-        this.manager = manager;
+        super(manager);
+    }
+
+    public void createGroup(GroupData group) {
+        openGroupPage();
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupsPage();
+    }
+
+    private void fillGroupForm(GroupData group) {
+        type(By.name("group_name"), group.name());
+        type(By.name("group_header"), group.header());
+        type(By.name("group_footer"), group.footer());
+    }
+
+    public void modifyGroup(GroupData modifiedGroup) {
+        openGroupPage();
+        selectGroup();
+        initGroupModification();
+        fillGroupForm(modifiedGroup);
+        submitGroupModification();
+        returnToGroupsPage();
+    }
+
+    public void removeGroup() {
+        openGroupPage();
+        selectGroup();
+        removeSelectedGroup();
+        returnToGroupsPage();
+    }
+
+    private void removeSelectedGroup() {
+        click(By.name("delete"));
+    }
+
+    private void returnToGroupsPage() {
+        click(By.linkText("group page"));
+    }
+
+    private void submitGroupModification() {
+        click(By.name("update"));
+    }
+
+    private void submitGroupCreation() {
+        click(By.name("submit"));
     }
 
     public void openGroupPage() {
         if (!manager.isElementPresent(By.name("new"))) {
-            manager.driver.findElement(By.linkText("groups")).click();
+            click(By.linkText("groups"));
         }
+    }
+
+    private void initGroupCreation() {
+        click(By.name("new"));
     }
 
     public boolean isGroupPresent() {
@@ -22,23 +70,13 @@ public class GroupHelper {
         return manager.isElementPresent(By.name("selected[]"));
     }
 
-    public void createGroup(GroupData group) {
-        openGroupPage();
-        manager.driver.findElement(By.name("new")).click();
-        manager.driver.findElement(By.name("group_name")).click();
-        manager.driver.findElement(By.name("group_name")).sendKeys(group.name());
-        manager.driver.findElement(By.name("group_header")).click();
-        manager.driver.findElement(By.name("group_header")).sendKeys(group.header());
-        manager.driver.findElement(By.name("group_footer")).click();
-        manager.driver.findElement(By.name("group_footer")).sendKeys(group.footer());
-        manager.driver.findElement(By.name("submit")).click();
-        manager.driver.findElement(By.linkText("group page")).click();
+    private void initGroupModification() {
+        click(By.name("edit"));
     }
 
-    public void removeGroup() {
-        openGroupPage();
-        manager.driver.findElement(By.name("selected[]")).click();
-        manager.driver.findElement(By.name("delete")).click();
-        manager.driver.findElement(By.linkText("group page")).click();
+    private void selectGroup() {
+        click(By.name("selected[]"));
     }
+
+
 }
